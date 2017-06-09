fd8b05e9def135ac034725f50f48ba415c753ea6
jcanabarro <joaovictorcana@gmail.com>
2017-06-08 21:35:31 -0300

    miss some functions

--- src/simulator/scheduler/Scheduler.java
+++ src/simulator/scheduler/Scheduler.java
@@ -2,23 +2,24 @@ package simulator.scheduler;
 
 public class Scheduler {
 
-    private PCB[] processControlBlock;
-    private int nProcess;
-    float timeLine = 0;
+    protected PCB[] processControlBlock;
+    protected int nProcess;
+    protected float timeLine = 0;
+    private float avgExecutionTime, avgWaitTime;
+    protected float normalization;
 
-    Scheduler(PCB[] pcb){
-        processControlBlock = pcb;
+
+    public Scheduler(PCB[] pcb){
+        this.processControlBlock = pcb;
+        this.nProcess = pcb.length;
+        normalization();
     }
 
-    /*void setPcb(int q, float arrivalTime, float waitTime, float remainingTime, float executed, double averageExecutionTime, double averageWaitTime, float beginTime, float endTime){
-        PCB[] temp  = processControlBlock;
-        nProcess++;
-        processControlBlock = new PCB[nProcess];
-        System.arraycopy(temp, 0, processControlBlock,0,temp.length);
-        processControlBlock[nProcess -1] = new PCB(q, arrivalTime, waitTime, remainingTime, executed, averageExecutionTime, averageWaitTime, beginTime, endTime);
-    }*/
+    public void normalization(){ // This function serve to set begin just only for the first process
+        normalization = processControlBlock[0].getArrivalTime();
+    }
 
-    private void sortPcb(){
+    public void sortPcb(){
         for (int i = 0; i < nProcess; i ++){
             for(int j = 1; j < nProcess; j++ ){
                 if (processControlBlock[j].getArrivalTime() < processControlBlock[j-1].getArrivalTime()){
@@ -30,27 +31,58 @@ public class Scheduler {
         }
     }
 
-    private void calcAll() {
-        for (int i = 0; i < nProcess; i++) {
-            processControlBlock[i].setBeginTime(timeLine);
-            timeLine += processControlBlock[i].getExecutionTime();
-            processControlBlock[i].setEndTime(timeLine);
+    public void averageTime(){
+        for(int i = 0; i < nProcess; i++) {
+            avgExecutionTime += processControlBlock[i].getBurstTime() + processControlBlock[i].getWaitTime();
+            avgWaitTime += processControlBlock[i].getWaitTime();
+        }
+        avgWaitTime /= nProcess;
+        avgExecutionTime /= nProcess;
+    }
+
+    public String identation(String text, int limit){
+        int qtdSpace = 0;
+        if( limit >= text.length()) {
+            qtdSpace = limit - text.length();
+            for (int i = 0; i < qtdSpace; i++)
+                text +=" ";
+            return text;
+        }else{
+            String temp = "";
+            for ( int i = 0; i < limit - 3; i++){
+                temp += text.charAt(i);
+            }
+            temp += "...";
+            return temp;
         }
     }
 
-    void firstComeFirstServed(){
-        sortPcb();
-        calcAll();
+    public void resultTable(){
+        System.out.print("-----------------------------------------------\n" +
+                         "| Process        | Wait Time | Execution Time |\n"+
+                         "-----------------------------------------------\n");
         for (int i = 0; i < nProcess; i++) {
-            processControlBlock[i].setExecuted(processControlBlock[i].getExecutionTime());
-            processControlBlock[i].setRemainingTime(0);
-            processControlBlock[i].setAverageExecutionTime(processControlBlock[i].getExecutionTime());
-            System.out.println("Executado: " + processControlBlock[i].getExecuted() + " Restante: " + processControlBlock[i].getRemainingTime() + " Tempo total:" + timeLine + " Begin: " + processControlBlock[i].getBeginTime() + " End: " + processControlBlock[i].getEndTime());
+            System.out.print("|" + identation(processControlBlock[i].getName(), 16) + "|" );
+            System.out.print(identation(String.valueOf(processControlBlock[i].getWaitTime()), 11) + "|" );
+            System.out.print(identation(String.valueOf(processControlBlock[i].getWaitTime() + processControlBlock[i].getExecuted()), 16) + "|\n" );
         }
+        System.out.print("+++++++++++++++++++++++++++++++++++++++++++++++\n");
+        System.out.print("|"+ identation("Average", 16) + "|");
+        System.out.print(identation(String.valueOf(avgWaitTime), 11) + "|" );
+        System.out.print(identation(String.valueOf(avgExecutionTime), 16) + "|\n" );
+        System.out.print("-----------------------------------------------\n");
+    }
+    public void preemptionTable( int i, int j, float time){
+        System.out.print("-- Preemption -- Current Time = "+ time+" --\n");
+        System.out.print("| Exits            | Enters           |\n");
+        System.out.print("---------------------------------------\n");
+        System.out.print("| "+identation(processControlBlock[i].getName(), 17)+"| "+identation(processControlBlock[j].getName(), 17)+"|\n");
+        System.out.print("---------------------------------------\n\n\n");
     }
 
-    public void roundRobin(){
-
-    }
+//    -- Preemption -- Current Time = 2.2 ---
+//    | Exits            | Enters           |
+//    ---------------------------------------
+//    | My Browser       | My Text Editor   |
+//    ---------------------------------------
 }
-
