9f6b03d2e6fa3439cebe76f02c409abee1675c9a
jcanabarro <joaovictorcana@gmail.com>
2017-06-11 15:55:21 -0300

    add format to output values

--- src/simulator/scheduler/Scheduler.java
+++ src/simulator/scheduler/Scheduler.java
@@ -1,5 +1,7 @@
 package simulator.scheduler;
 
+import javafx.animation.Timeline;
+
 class Scheduler {
     PCB[] processControlBlock;
     int nProcess;
@@ -40,7 +42,7 @@ class Scheduler {
         avgExecutionTime /= nProcess;
     }
 
-    private String identation (String text, int limit) {
+    private String indentation(String text, int limit) {
         int qtdSpace = 0;
         if (limit >= text.length()) {
             qtdSpace = limit - text.length();
@@ -63,26 +65,26 @@ class Scheduler {
                 "| Process        | Wait Time | Execution Time | Arrival Time | Burst Time |\n" +
                 "---------------------------------------------------------------------------\n");
         for (int i = 0; i < nProcess; i++) {
-            System.out.print("|" + identation(processControlBlock[i].getName(), 16) + "|");
-            System.out.print(identation(String.valueOf(processControlBlock[i].getWaitTime()), 11) + "|");
-            System.out.print(identation(String.valueOf(processControlBlock[i].getWaitTime() + processControlBlock[i].getExecuted()), 16) + "|");
-            System.out.print(identation(String.valueOf(processControlBlock[i].getArrivalTime()), 14) + "|");
-            System.out.print(identation(String.valueOf(processControlBlock[i].getBurstTime()), 12) + "|\n");
+            System.out.print("|" + indentation(processControlBlock[i].getName(), 16) + "|");
+            System.out.print(indentation(String.valueOf(String.format("%.3f",processControlBlock[i].getWaitTime())), 11) + "|");
+            System.out.print(indentation(String.valueOf(String.format("%.3f",processControlBlock[i].getWaitTime() + processControlBlock[i].getExecuted())), 16) + "|");
+            System.out.print(indentation(String.valueOf(String.format("%.3f",processControlBlock[i].getArrivalTime())), 14) + "|");
+            System.out.print(indentation(String.valueOf(String.format("%.3f",processControlBlock[i].getBurstTime())), 12) + "|\n");
         }
         System.out.print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-        System.out.print("|" + identation("Average", 16) + "|");
-        System.out.print(identation(String.valueOf(avgWaitTime), 11) + "|");
-        System.out.print(identation(String.valueOf(avgExecutionTime), 16) + "|");
-        System.out.println(identation(" -", 27) + "|");
+        System.out.print("|" + indentation("Average", 16) + "|");
+        System.out.print(indentation(String.valueOf(String.format("%.3f",avgWaitTime)), 11) + "|");
+        System.out.print(indentation(String.valueOf(String.format("%.3f",avgExecutionTime)), 16) + "|");
+        System.out.println(indentation("Total Burst", 14) + "|" + indentation(String.valueOf(String.format("%.3f",this.timeLine)), 12) +"|");
         System.out.print("---------------------------------------------------------------------------\n\n");
     }
 
     void preemptionTable (int i, int j, float time) {
-        System.out.println("\n-> Context Change at time = " + time);
+        System.out.println("\n-> Context Change at time = " + String.format("%.3f",time));
         System.out.println("---------------------------------------");
         System.out.println("| Exits            | Enters           |");
         System.out.println("---------------------------------------");
-        System.out.println("| " + identation(processControlBlock[i].getName(), 17) + "| " + identation(processControlBlock[j].getName(), 17) + "|");
+        System.out.println("| " + indentation(processControlBlock[i].getName(), 17) + "| " + indentation(processControlBlock[j].getName(), 17) + "|");
         System.out.println("---------------------------------------\n");
     }
 }
